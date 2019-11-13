b58535117a3a6c505c817dda02938a20082f4fca
Andrea Bender <andreab.1726@gmail.com>
2019-11-13 15:28:57 -0700

    issue with user_name, but tests are passing

---
+++ src/auth/auth-service.js
@@ -0,0 +1,17 @@
+/* eslint-disable indent */
+/* eslint-disable strict */
+const AuthService = {
+    getUserWithUserName(db, user_name) {
+        return db('thingful_users')
+            .where({ user_name })
+            .first();
+    },
+    parseBasicToken(token) {
+        return Buffer
+            .from(token, 'base64')
+            .toString()
+            .split(':');
+    },
+};
+
+module.exports = AuthService;
